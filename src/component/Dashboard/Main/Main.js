e7d1e62b5a2ee3a4a402140247224332370d268f
Fehér Erik <fehererik2001@gmail.com>
2023-03-30 20:13:42 +0200

    Fix - deleting leftover text

--- src/component/Dashboard/Main/Main.js
+++ src/component/Dashboard/Main/Main.js
@@ -1,31 +1,34 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import EndLoader from "../../EndLoader";
 
 function Main() {
-
+  const [channel, setChannel] = useState(localStorage.getItem('username').toLowerCase())
+  const [siteUrl, setSiteUrl] = useState("")
 
   useEffect(()=>{
-
+    if(window.location.href.includes("localhost")){
+      setSiteUrl("localhost")
+    } else{
+      setSiteUrl(process.env.REACT_APP_FRONTEND_URI.replace("https://",""))
+    }
   },[])
+
+
 
     return (
       <>
       <div className="flex w-full h-full">
         <iframe
           className="w-3/4"
-          src={`https://player.twitch.tv/?channel=${localStorage.getItem('username').toLowerCase()}&parent=localhost`}>
+          src={`https://player.twitch.tv/?channel=${channel}&parent=${siteUrl}`}>
         </iframe>
         <iframe id="twitch-chat-embed"
           className="w-1/4"
-          src={`https://www.twitch.tv/embed/${localStorage.getItem('username').toLowerCase()}/chat?parent=${process.env.REACT_APP_FRONTEND_LOCAL_URI.replace("http://","").replace(":3000","")}`}
+          src={`https://www.twitch.tv/embed/${channel}/chat?parent=${siteUrl}`}
           sandbox="allow-scripts allow-popups allow-popups-to-escape-sandbox allow-modals allow-same-origin"
           >
         </iframe>
-      </div>
-      <div>
-        <card>
-          adssa
-        </card>
       </div>
       <EndLoader/>
       </>
